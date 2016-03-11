37dbac3fe396dc2ebd42e901fef47c6c23886182
ianchildress <ianchildress@gmail.com>
2016-03-11 10:53:12 -0800

    Added debug option

--- main.go
+++ main.go
@@ -16,17 +16,19 @@ func main() {
 	var (
 		targetPort string
 		targetURL  string
+		debug      bool
 	)
 
-	flag.StringVar(&targetPort, "port", defaultPort, "port the proxy will listen on. Example: 80")
-	flag.StringVar(&targetURL, "target", defaultTarget, "url to forward the request to. Example: http://127.0.0.1:8080")
+	flag.StringVar(&targetPort, "port", defaultPort, "port the proxy will listen on. Example: -port 80")
+	flag.StringVar(&targetURL, "target", defaultTarget, "url to forward the request to. Example: -target http://127.0.0.1:8080")
+	flag.BoolVar(&debug, "debug", false, "Turn debug information on/off. Example: -debug true")
 	flag.Parse()
 
 	// Validate port and target formats here
 
 	// Create the reverse proxy
 	proxy := new(SingleProxy)
-	proxy.New(targetURL)
+	proxy.New(targetURL, debug)
 
 	http.HandleFunc("/", proxy.handle)
 	http.ListenAndServe(":"+targetPort, nil)
--- singleProxy.go
+++ singleProxy.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
+// SingleProxy contains our *httputil.ReverseProxy settings.
 type SingleProxy struct {
 	target *url.URL
 	proxy  *httputil.ReverseProxy
+	debug  bool
 }
 
 // New returns a simpleProxy
-func (p *SingleProxy) New(target string) *SingleProxy {
+func (p *SingleProxy) New(target string, debug bool) *SingleProxy {
 	url, _ := url.Parse(target)
-	return &SingleProxy{target: url, proxy: httputil.NewSingleHostReverseProxy(url)}
+	return &SingleProxy{target: url, proxy: httputil.NewSingleHostReverseProxy(url), debug: debug}
 }
 
 func (p *SingleProxy) handle(w http.ResponseWriter, r *http.Request) {
+	if p.debug == true {
+		log.Println("Incoming request from ", r.RemoteAddr, "for uri", r.RequestURI)
+	}
 	w.Header().Set("X-goReverseProxy", "goReverseProxy")
 	p.proxy.ServeHTTP(w, r)
 }
